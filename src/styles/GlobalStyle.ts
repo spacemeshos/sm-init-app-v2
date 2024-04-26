ead720f16492e18ebae2e9c0c0a4317938fae670
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-04-27 00:33:58 +0200

    added styles

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body, html {
+    height: 100%;
+    
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+export default GlobalStyle;
