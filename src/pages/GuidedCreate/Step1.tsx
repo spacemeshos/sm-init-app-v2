741cfb6f942f4a7707118a1816ffbc50c93d002a
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-04-27 00:42:08 +0200

    copied pages from my repo

---
+++ src/pages/GuidedCreate/Step1.tsx
@@ -0,0 +1,148 @@
+import * as React from "react";
+import styled from "styled-components";
+import { BackButton, ForwardButton, TooltipButton } from "../../components/button";
+import Title from "../../components/titles";
+import Colors from "../../styles/colors";
+import ProgressBar from "../../components/progress";
+import Tile from "../../components/tile";
+import { ExternalLinks } from "../../Shared/Constants";
+
+const NavProgress = styled.div`
+  width: 1200px;
+  height: 160px;
+  position: abslute;
+`;
+
+const TextWrapper = styled.div`
+  width: 1200px;
+  height: 22px;
+  top: 170px;
+
+  position: absolute;
+  color: ${Colors.grayDark};
+  font-family: "Source Code Pro", monospace;
+  font-optical-sizing: auto;
+  font-weight: 100;
+  font-height: 16px;
+  font-style: normal;
+  height: 22px;
+  text-align: center;
+  font-size: 16px;
+  font-weight: 100;
+  letter-spacing: 2px;
+`;
+
+const ContainerBottom = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: flex-start;
+  top: 278px;
+  width: 1200px;
+  position: absolute;
+`;
+
+const HintWrapper = styled.div`
+  border: 1px solid transparent;
+  background-color: ${Colors.darkerGreen};
+  transition: border-color 0.3s ease;
+  height: 370px;
+  width: 450px;
+  left: 637px;
+  position: absolute;
+
+  display: flex;
+  justify-content: center;
+
+  /* Gradient border */
+  border-image: linear-gradient(${Colors.greenLight}, ${Colors.greenDark});
+  border-image-slice: 1;
+
+  @media (max-width: 991px) {
+    white-space: initial;
+  }
+`;
+const HintImage = styled.img`
+  aspect-ratio: 1;
+  object-fit: contain;
+  width: 100px;
+  position: absolute;
+  top: 41px;
+  left: 175px;
+`;
+const HintText = styled.div`
+  width: 450px;
+  height: 228px;
+  top: 141px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+  color: ${Colors.greenLight};
+  font-family: "Source Code Pro", monospace;
+  font-weight: 100;
+  font-height: 16px;
+  text-align: center;
+  font-size: 22px;
+  font-weight: 100;
+  line-height: 35px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+`;
+
+const Step1: React.FC = () => {
+  const folder = require("../../assets/folder.png");
+  const bulb = require("../../assets/light-bulb.png");
+  const navigateToDocs = () => window.open(ExternalLinks.Docs);
+  const navigateToDiscord = () => window.open(ExternalLinks.Discord);
+
+  return (
+    <>
+      <NavProgress>
+        <ProgressBar progress={33} />
+        <BackButton />
+        <ForwardButton />
+      </NavProgress>
+      <TextWrapper>
+        <Title text="Your Path to Crypto Starts Here" />
+        Whenever you feel lost, remember to read the tips,{" "}
+        <a
+          onClick={navigateToDocs}
+          role="button"
+          tabIndex={0}
+          style={{ cursor: "pointer", textDecoration: "underline" }}
+        >
+          Spacemesh docs
+        </a>
+        {", and "}
+        <a
+          onClick={navigateToDiscord}
+          role="button"
+          tabIndex={0}
+          style={{ cursor: "pointer", textDecoration: "underline" }}
+        >
+          Discord FAQ
+        </a>
+      </TextWrapper>
+      <ContainerBottom>
+        <Tile
+          text={"Where to Store pos data?"}
+          imageSrc={folder}
+          buttonText="Choose directory"
+          onClick={() => console.log("Button Clicked")} /* TO DO */
+        />
+        <HintWrapper>
+          <HintImage src={bulb} />
+          <HintText>
+            Next, we'll optimize your settings with a quick benchmark to boost
+            your rewards and qualification chances every epoch
+          </HintText>
+          <TooltipButton/> {/* TO DO */}
+        </HintWrapper>
+      </ContainerBottom>
+    </>
+  );
+};
+
+export default Step1;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,159 @@
+import * as React from "react";
+import styled from "styled-components";
+import { Button } from "../components/button";
+import Title from "../components/titles";
+import Colors from "../styles/colors";
+import { HoverAccordionMenu } from "../components/accordion";
+import { ExternalLinks } from "../Shared/Constants";
+import { useNavigate } from "react-router-dom";
+
+const ContainerLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 450px;
+  height: 740px;
+  position: relative;
+`;
+
+const ContainerRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: flex-start;
+  top: 220px;
+  left: 520px;
+  width: 600px;
+  position: absolute;
+`;
+const CustomTitle = styled.h1`
+  width: 1200px;
+  height: 21px;
+  top: 50px;
+  position: absolute;
+  color: ${Colors.grayLight};
+  font-family: "Source Code Pro", sans-serif;
+  text-align: center;
+  white-space: nowrap;
+  text-transform: uppercase;
+  font-size: 28px;
+  font-weight: 100;
+  letter-spacing: 5px;
+`;
+
+const Image = styled.img`
+  width: auto;
+  height: 215px;
+  top: 196px;
+  position: relative;
+`;
+
+const TextWrapper = styled.div`
+  width: 300px;
+  top: 443px;
+  position: absolute;
+`;
+
+const PrereqButton = styled.div`
+  height: 60px;
+  top: 500px;
+  width: 300px;
+  position: absolute;
+`;
+
+const VerticalDivider = styled.div`
+  background: linear-gradient(
+    to top,
+    ${Colors.purpleLight},
+    ${Colors.purpleDark}
+  );
+  border-image-slice: 1;
+  margin-top: 150px;
+  left: 450px;
+  height: 540px;
+  width: 1px;
+  position: absolute;
+`;
+
+const Home: React.FC = () => {
+  const LogoWhite = require("../assets/transparentbg.gif");
+  const Add = require("../assets/plus.png");
+  const Edit = require("../assets/edit.png");
+  const Check = require("../assets/check.png");
+  const [hoveredMenu, setHoveredMenu] = React.useState<string | null>(null);
+  const navigate = useNavigate();
+
+  const navigateToReqs = () => window.open(ExternalLinks.Requirements);
+
+  const CreateButtons = [
+    { label: "Guided Mode", onClick: () => navigate("/guided/step1") },
+    { label: "Advanced Mode", onClick: () => console.log("Button 2 clicked") },
+  ];
+  const EditButtons = [
+    { label: "POS data Size", onClick: () => console.log("Button 1 clicked") },
+    {
+      label: "Proving Settings",
+      onClick: () => console.log("Button 2 clicked"),
+    },
+  ];
+  const CheckButtons = [
+    {
+      label: "Generation speed",
+      onClick: () => console.log("Button 1 clicked"),
+    },
+    {
+      label: "Proving Capacity",
+      onClick: () => console.log("Button 2 clicked"),
+    },
+    {
+      label: "POS data Validity",
+      onClick: () => console.log("Button 2 clicked"),
+    },
+  ];
+
+  return (
+    <>
+      <CustomTitle>PROOF OF SPACE-TIME MANAGER</CustomTitle>
+
+      <ContainerLeft>
+        <Image src={LogoWhite} />
+        <VerticalDivider />
+        <TextWrapper>
+          <Title text="ARE YOU READY?" />
+        </TextWrapper>
+        <PrereqButton>
+          <Button onClick={navigateToReqs} label="Check prerequisites" />
+        </PrereqButton>
+      </ContainerLeft>
+      <ContainerRight>
+        <HoverAccordionMenu
+          $isHovered={hoveredMenu === "generate"}
+          onMouseEnter={() => setHoveredMenu("generate")}
+          onMouseLeave={() => setHoveredMenu(null)}
+          imageSrc={Add}
+          title="Generate POS Data"
+          buttons={CreateButtons}
+        />
+        <HoverAccordionMenu
+          $isHovered={hoveredMenu === "edit"}
+          onMouseEnter={() => setHoveredMenu("edit")}
+          onMouseLeave={() => setHoveredMenu(null)}
+          imageSrc={Edit}
+          title="Edit"
+          buttons={EditButtons}
+        />
+        <HoverAccordionMenu
+          $isHovered={hoveredMenu === "check"}
+          onMouseEnter={() => setHoveredMenu("check")}
+          onMouseLeave={() => setHoveredMenu(null)}
+          imageSrc={Check}
+          title="Check"
+          buttons={CheckButtons}
+        />
+      </ContainerRight>
+    </>
+  );
+};
+
+export default Home;
