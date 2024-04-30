611450927600a12737b12e8e3d515267caa91fdb
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-04-30 11:02:01 +0200

    configuring yarn lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,45 @@
+import ParserTypescriptEslint from "@typescript-eslint/parser";
+import PluginImport from "eslint-plugin-import";
 import globals from "globals";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
 import path from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import pluginJs from "@eslint/js";
 
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = path.dirname(__fileName);
+const compat = new FlatCompat({
+  baseDirectory: __dirName,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  ...compat.extends("standard-with-typescript"),
-  pluginReactConfig,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      parser: ParserTypescriptEslint,
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: __dirName,
+      },
+    },
+    plugins: {
+      import: PluginImport,
+    },
+    settings: {
+      "import/resolver": {
+        ...PluginImport.configs.typescript.settings["import/resolver"],
+        typescript: {
+          project: ["tsconfig.json"],
+        },
+      },
+    },
+  },
+  { ignores: ["**/dist/*", "**/node_modules/*", "eslint.config.mjs"] },
 ];
