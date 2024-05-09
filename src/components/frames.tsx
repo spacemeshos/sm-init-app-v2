11296afc8f9edf3cd116f809f4f47d8ef102f8a0
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-05-09 11:29:43 +0200

    separate image from frame

--- src/components/frames.tsx
+++ src/components/frames.tsx
@@ -6,7 +6,6 @@ interface FrameProps {
   heading?: string;
   subheader?: string;
   footer?: string;
-  imageSrc?: string;
   children?: React.ReactNode;
   buttonText?: string;
   onClick?: () => void;
@@ -72,21 +71,6 @@ const SubHeader = styled.h3`
   width: 100%;
 `;
 
-const Image = styled.img<{
-  top?: number;
-  left?: number;
-  height?: number;
-  width?: number;
-}>`
-  aspect-ratio: 1;
-  object-fit: contain;
-  position: absolute;
-  top: ${({ top }) => top}px;
-  left: ${({ left }) => left}px;
-  height: ${({ height }) => height}px;
-  width: ${({ width }) => width}px;
-`;
-
 const Footer = styled.h3`
   color: ${Colors.grayLight};
   font-family: "Source Code Pro ExtraLight", sans-serif, Arial;
@@ -103,7 +87,6 @@ const Footer = styled.h3`
 const Frame: React.FC<FrameProps> = ({
   heading,
   subheader,
-  imageSrc,
   footer,
   children,
   wrapperHeight,
@@ -122,7 +105,6 @@ const Frame: React.FC<FrameProps> = ({
     >
       <FrameHeading>{heading}</FrameHeading>
       <SubHeader>{subheader}</SubHeader>
-      {imageSrc && <Image src={imageSrc} alt="Descriptive text here" />}
       <Footer>{footer}</Footer>
       {children}
     </Wrapper>
