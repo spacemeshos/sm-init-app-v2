6ed1235e18e79e3ddc899b3483b1f3ff88caf97c
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-05-09 11:37:32 +0200

    clean up

--- src/components/titles.tsx
+++ src/components/titles.tsx
@@ -8,6 +8,41 @@ interface TitleProps {
   left?: number;
 }
 
+const StyledTitle = styled.h1<{
+  top?: number;
+  left?: number;
+}>`
+  color: ${Colors.grayLight};
+  font-family: "Source Code Pro", sans-serif;
+  text-align: ${({ left }) => (left == null ? "center" : "left")};
+  white-space: nowrap;
+  text-transform: uppercase;
+  font-size: 21px;
+  font-weight: 400;
+  letter-spacing: 4px;
+  top: ${({ top }) => top}px;
+  left: ${({ left }) => (left == null ? "50%" : `${left}px`)};
+  transform: ${({ left }) => (left == null ? "translateX(-50%)" : "none")};
+  position: absolute;
+`;
+
+const StyledSubheader = styled.h2<{
+  top?: number;
+  left?: number;
+}>`
+  color: ${Colors.grayLight};
+  font-family: "Source Code Pro Extralight";
+  text-align: ${({ left }) => (left == null ? "center" : "left")};
+  white-space: nowrap;
+  text-transform: uppercase;
+  font-size: 18px;
+  font-weight: 100;
+  letter-spacing: 4px;
+  top: ${({ top }) => top}px;
+  left: ${({ left }) => (left == null ? "50%" : `${left}px`)};
+  transform: ${({ left }) => (left == null ? "translateX(-50%)" : "none")};
+  position: absolute;
+`;
 const Title: React.FC<TitleProps> = ({ text, top, left }) => {
   return (
     <StyledTitle top={top} left={left}>
@@ -15,24 +50,6 @@ const Title: React.FC<TitleProps> = ({ text, top, left }) => {
     </StyledTitle>
   );
 };
-
-const StyledTitle = styled.h1<{
-  top?: number;
-  left?: number;
-}>`
-  color: ${Colors.grayLight};
-  font-family: "Source Code Pro", sans-serif;
-  text-align: ${({ left }) => left == null ? 'center' : 'left'};
-    white-space: nowrap;
-  text-transform: uppercase;
-  font-size: 21px;
-  font-weight: 400;
-  letter-spacing: 4px;
-  top: ${({ top }) => top}px;
-  left: ${({ left }) => left == null ? '50%' : `${left}px`};
-  transform: ${({ left }) => left == null ? 'translateX(-50%)' : 'none'};
-  position: absolute;
-`;
 
 const Subheader: React.FC<TitleProps> = ({ text, top, left }) => {
   return (
@@ -42,24 +59,4 @@ const Subheader: React.FC<TitleProps> = ({ text, top, left }) => {
   );
 };
 
-const StyledSubheader = styled.h2<{
-  top?: number;
-  left?: number;
-}>`
-  color: ${Colors.grayLight};
-  font-family: "Source Code Pro Extralight";
-  text-align: ${({ left }) => left == null ? 'center' : 'left'};
-    white-space: nowrap;
-  text-transform: uppercase;
-  font-size: 21px;
-  font-weight: 400;
-  letter-spacing: 4px;
-  top: ${({ top }) => top}px;
-  left: ${({ left }) => left == null ? '50%' : `${left}px`};
-  transform: ${({ left }) => left == null ? 'translateX(-50%)' : 'none'};
-  position: absolute;
-`;
-
-
-
-export {Title, Subheader};
+export { Title, Subheader };
