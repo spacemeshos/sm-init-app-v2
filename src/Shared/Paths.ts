b764cd8ea8c3f69a66f8ab7f6fdb3fbb86e9e472
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-05-14 04:20:31 +0200

    prep for select direcotry button

--- src/Shared/Paths.ts
+++ src/Shared/Paths.ts
@@ -2,6 +2,6 @@ export enum GuidedCreate {
     // Screens
     SelectDirectory = 'src/pages/GuidedCreate/SelectDirectory.tsx',
     TailoredSettings = 'src/pages/GuidedCreate/TailoredSettings.tsx',
-    Summary = "src/pages/GuidedCreate/Summary.tsx"
-
+    Summary = "src/pages/GuidedCreate/Summary.tsx",
+    DirectorySelectionModal="src/components/dir_select_modal.tsx"
   }
--- src/Shared/Routes.tsx
+++ src/Shared/Routes.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import SelectDirectory from '../pages/GuidedCreate/SelectDirectory';
-import TailoredSettings from '../pages/GuidedCreate/TailoredSettings';
-import Summary from '../pages/GuidedCreate/Summary';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import SelectDirectory from "../pages/GuidedCreate/SelectDirectory";
+import TailoredSettings from "../pages/GuidedCreate/TailoredSettings";
+import Summary from "../pages/GuidedCreate/Summary";
+import DirectorySelectionModal from "../components/dir_select_modal";
 
 const AppRoutes = () => (
   <BrowserRouter>
@@ -11,6 +12,20 @@ const AppRoutes = () => (
       <Route path="/guided/SelectDirectory" element={<SelectDirectory />} />
       <Route path="/guided/TailoredSettings" element={<TailoredSettings />} />
       <Route path="/guided/Summary" element={<Summary />} />
+      <Route
+        path="/guided/src/components/dir_select_modal.tsx"
+        element={
+          <DirectorySelectionModal
+            isOpen={false}
+            onSelect={function (directory: string): void {
+              throw new Error("Function not implemented.");
+            }}
+            onClose={function (): void {
+              throw new Error("Function not implemented.");
+            }}
+          />
+        }
+      />
     </Routes>
   </BrowserRouter>
 );
