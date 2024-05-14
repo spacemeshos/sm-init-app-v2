524e983f79cbb363ff667ec574994b79f67d2365
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-05-14 02:41:56 +0200

    prep for backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,19 @@
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![run_cli_command])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+#[tauri::command]
+fn run_cli_command(args: Vec<String>) -> Result<String, String> {
+    use std::process::Command;
+    let output = Command::new("../postcli/postcli")
+        .args(&args)
+        .output();
+
+    match output {
+        Ok(output) => Ok(String::from_utf8_lossy(&output.stdout).to_string()),
+        Err(e) => Err(e.to_string()),
+    }
 }
