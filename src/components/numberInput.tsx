83909587792b5ae0eace01646fa94038cf398587
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-05-21 00:24:42 +0200

    adjust the styling

--- src/components/numberInput.tsx
+++ src/components/numberInput.tsx
@@ -1,15 +1,16 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   Unstable_NumberInput as BaseNumberInput,
   NumberInputProps,
-} from '@mui/base/Unstable_NumberInput';
-import { styled } from '@mui/system';
-import RemoveIcon from '@mui/icons-material/Remove';
-import AddIcon from '@mui/icons-material/Add';
+} from "@mui/base/Unstable_NumberInput";
+import { styled } from "@mui/system";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
+import Colors from "../styles/colors";
 
 const NumberInput = React.forwardRef(function CustomNumberInput(
   props: NumberInputProps,
-  ref: React.ForwardedRef<HTMLDivElement>,
+  ref: React.ForwardedRef<HTMLDivElement>
 ) {
   return (
     <BaseNumberInput
@@ -22,7 +23,7 @@ const NumberInput = React.forwardRef(function CustomNumberInput(
       slotProps={{
         incrementButton: {
           children: <AddIcon fontSize="small" />,
-          className: 'increment',
+          className: "increment",
         },
         decrementButton: {
           children: <RemoveIcon fontSize="small" />,
@@ -38,90 +39,55 @@ export default function QuantityInput() {
   return <NumberInput aria-label="Quantity Input" min={1} max={99} />;
 }
 
-const blue = {
-  100: '#daecff',
-  200: '#b6daff',
-  300: '#66b2ff',
-  400: '#3399ff',
-  500: '#007fff',
-  600: '#0072e5',
-  700: '#0059B2',
-  800: '#004c99',
-};
-
-const grey = {
-  50: '#F3F6F9',
-  100: '#E5EAF2',
-  200: '#DAE2ED',
-  300: '#C7D0DD',
-  400: '#B0B8C4',
-  500: '#9DA8B7',
-  600: '#6B7A90',
-  700: '#434D5B',
-  800: '#303740',
-  900: '#1C2025',
-};
-
-const StyledInputRoot = styled('div')(
-  ({ theme }) => `
-  font-family: 'IBM Plex Sans', sans-serif;
+const StyledInputRoot = styled("div")`
+  font-family: "Source Code Pro", sans-serif;
   font-weight: 400;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[500]};
+  color: ${Colors.grayLight};
   display: flex;
   flex-flow: row nowrap;
   justify-content: center;
   align-items: center;
-`,
-);
+  z-index: auto;
+  height: 45px;
+`;
 
-const StyledInput = styled('input')(
-  ({ theme }) => `
-  font-size: 0.875rem;
-  font-family: inherit;
+const StyledInput = styled("input")`
+  font-size: 15px;
+  font-family: "Source Code Pro", sans-serif;
   font-weight: 400;
-  line-height: 1.375;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-  box-shadow: 0px 2px 4px ${
-    theme.palette.mode === 'dark' ? 'rgba(0,0,0, 0.5)' : 'rgba(0,0,0, 0.05)'
-  };
-  border-radius: 8px;
+  height: 50px;
+  color: ${Colors.grayLight};
+  background: ${Colors.background};
+  border: 2px solid ${Colors.darkerPurple};
   margin: 0 8px;
-  padding: 10px 12px;
-  outline: 0;
-  min-width: 0;
-  width: 4rem;
+  padding: 0px 12px;
+  min-width: 130px;
   text-align: center;
 
   &:hover {
-    border-color: ${blue[400]};
+    border-color: ${Colors.purpleDark};
   }
 
   &:focus {
-    border-color: ${blue[400]};
-    box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[700] : blue[200]};
+    border-color: ${Colors.purpleDark};
   }
 
   &:focus-visible {
     outline: 0;
   }
-`,
-);
+`;
 
-const StyledButton = styled('button')(
-  ({ theme }) => `
-  font-family: 'IBM Plex Sans', sans-serif;
+const StyledButton = styled("button")`
+  font-family: "Source Code Pro", sans-serif;
   font-size: 0.875rem;
   box-sizing: border-box;
   line-height: 1.5;
   border: 1px solid;
-  border-radius: 999px;
-  border-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-  color: ${theme.palette.mode === 'dark' ? grey[200] : grey[900]};
-  width: 32px;
-  height: 32px;
+  border-color: ${Colors.purpleDark};
+  background: ${Colors.purpleDark};
+  color: ${Colors.purpleLight};
+  width: 50px;
+  height: 50px;
   display: flex;
   flex-flow: row nowrap;
   justify-content: center;
@@ -132,9 +98,8 @@ const StyledButton = styled('button')(
 
   &:hover {
     cursor: pointer;
-    background: ${theme.palette.mode === 'dark' ? blue[700] : blue[500]};
-    border-color: ${theme.palette.mode === 'dark' ? blue[500] : blue[400]};
-    color: ${grey[50]};
+    background: ${Colors.purpleDark};
+    color: ${Colors.grayLight};
   }
 
   &:focus-visible {
@@ -144,5 +109,4 @@ const StyledButton = styled('button')(
   &.increment {
     order: 1;
   }
-`,
-);
+`;
