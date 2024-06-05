06ecac23a182306fd26d22710a5411ceb84e5d84
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-06-05 21:05:49 +0200

    refactoring to reuse setup code

--- src/pages/GuidedCreate/editCPU.tsx
+++ src/pages/GuidedCreate/editCPU.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Colors from "../../styles/colors";
-import Tile from "../../components/tile";
-import CustomNumberInput from "../../components/input";
-import { SaveButton, CancelButton } from "../../components/button";
+import { SetupProving } from "../../components/setupPOS";
 
 const Backdrop = styled.div`
   width: 100%;
@@ -47,16 +45,6 @@ const Subheader = styled.h2`
   padding: 10px 75px 10px;
 `;
 
-const BgImage = styled.img`
-  aspect-ratio: 1;
-  object-fit: contain;
-  width: 250px;
-  position: absolute;
-  left: 30px;
-  top: 30px;
-  opacity: 0.03;
-`;
-
 const CloseButton = styled.button`
   position: absolute;
   top: 10px;
@@ -67,37 +55,6 @@ const CloseButton = styled.button`
   cursor: pointer;
   font-size: 28px;
 `;
-
-const BottomContainer = styled.div`
-  height: 400px;
-  width: 1200px;
-  position: absolute;
-  top: 260px;
-  left: 0px;
-  display: flex;
-  justify-content: space-evenly;
-  flex-direction: row;
-`;
-
-const TileWrapper = styled.div`
-  height: 370px;
-  width: 450px;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-  flex-direction: row;
-`;
-
-const SelectedValue = styled.h1`
-  color: ${Colors.greenLight};
-  font-family: "Source Code Pro ExtraLight", sans-serif;
-  font-weight: 300;
-  font-size: 50px;
-  position: relative;
-`;
-
 type Props = {
   onClose: () => void;
   isOpen: boolean;
@@ -105,96 +62,19 @@ type Props = {
 };
 
 const CPUedit = ({ onClose, isOpen }: Props) => {
-  const [cpuValue, setCpuValue] = useState(8); //placeholder
-  const [noncesValue, setNoncesValue] = useState(288);
-  const [isCpuInputVisible, setIsCpuInputVisible] = useState(true);
-  const [isNoncesInputVisible, setIsNoncesInputVisible] = useState(true);
 
   if (!isOpen) return null;
-  const cpu = require("../../assets/cpu.png");
-
-  const handleCancelCpu = () => {
-    setCpuValue(8); // Reset to default value
-    setIsCpuInputVisible(true);
-  };
-
-  const handleSaveCpu = () => {
-    setIsCpuInputVisible(false);
-  };
-
-  const handleCancelNonces = () => {
-    setNoncesValue(288); // Reset to default value
-    setIsNoncesInputVisible(true);
-  };
-
-  const handleSaveNonces = () => {
-    setIsNoncesInputVisible(false);
-  };
 
   return (
     <Backdrop onClick={onClose}>
       <Wrapper onClick={(e) => e.stopPropagation()}>
-        <BgImage src={cpu} />
         <CloseButton onClick={onClose}>&times;</CloseButton>
         <Header>How to prepare POST proofs </Header>
         <Subheader>
           Your CPU will be utilized once every two weeks to complete POET
           proving. Depending on your settings, it might take several hours.
         </Subheader>
-        <BottomContainer>
-          <TileWrapper>
-            <Tile
-              heading="Select number of CPU cores"
-              footer="more CPU cores -> faster proof generation"
-            />
-            {isCpuInputVisible ? (
-              <>
-                <CustomNumberInput
-                  min={1}
-                  max={16}
-                  step={1}
-                  value={cpuValue}
-                  fontsize={36}
-                  height={80}
-                  onChange={(val) => setCpuValue(val)}
-                />
-                <SaveButton buttonLeft={55} onClick={handleSaveCpu} />
-                <CancelButton buttonLeft={45} onClick={handleCancelCpu} />
-              </>
-            ) : (
-              <>
-                <SelectedValue>{cpuValue}</SelectedValue>
-                <CancelButton buttonLeft={50} onClick={handleCancelCpu} />
-              </>
-            )}
-          </TileWrapper>
-          <TileWrapper>
-            <Tile
-              heading="Select number of Nonces"
-              footer="more nonces -> more likely proof generated on the first try"
-            />
-            {isNoncesInputVisible ? (
-              <>
-                <CustomNumberInput
-                  min={16}
-                  max={9999}
-                  step={16}
-                  value={noncesValue}
-                  fontsize={36}
-                  height={80}
-                  onChange={(val) => setNoncesValue(val)}
-                />
-                <SaveButton buttonLeft={55} onClick={handleSaveNonces} />
-                <CancelButton buttonLeft={45} onClick={handleCancelNonces} />
-              </>
-            ) : (
-              <>
-                <SelectedValue>{noncesValue}</SelectedValue>
-                <CancelButton buttonLeft={50} onClick={handleCancelNonces} />
-              </>
-            )}
-          </TileWrapper>
-        </BottomContainer>
+        <SetupProving/>
       </Wrapper>
     </Backdrop>
   );
--- src/pages/GuidedCreate/editSize.tsx
+++ src/pages/GuidedCreate/editSize.tsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Colors from "../../styles/colors";
-import Tile from "../../components/tile";
-import { CancelButton, SaveButton } from "../../components/button";
-import CustomNumberInput from "../../components/input";
+import { SetupSize } from "../../components/setupPOS";
 
 const Backdrop = styled.div`
   width: 100%;
@@ -50,16 +48,6 @@ const Subheader = styled.h2`
   padding: 0px 75px 10px;
 `;
 
-const BgImage = styled.img`
-  aspect-ratio: 1;
-  object-fit: contain;
-  width: 250px;
-  position: absolute;
-  left: 20px;
-  top: 20px;
-  opacity: 0.02;
-`;
-
 const CloseButton = styled.button`
   position: absolute;
   top: 10px;
@@ -71,36 +59,6 @@ const CloseButton = styled.button`
   font-size: 28px;
 `;
 
-const BottomContainer = styled.div`
-  height: 400px;
-  width: 1200px;
-  position: absolute;
-  top: 260px;
-  left: 0px;
-  display: flex;
-  justify-content: space-evenly;
-  flex-direction: row;
-`;
-
-const TileWrapper = styled.div`
-  height: 370px;
-  width: 450px;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-  flex-direction: row;
-`;
-
-const SelectedValue = styled.h1`
-  color: ${Colors.greenLight};
-  font-family: "Source Code Pro ExtraLight", sans-serif;
-  font-weight: 300;
-  font-size: 50px;
-  position: relative;
-`;
-
 type Props = {
   onClose: () => void;
   isOpen: boolean;
@@ -108,32 +66,8 @@ type Props = {
 };
 
 const editSize = ({ onClose, isOpen }: Props) => {
-  const [dataSizeValue, setPOSsizeValue] = useState(256);
-  const [fileSizeValue, setFileSizeValue] = useState(4096);
-  const [isPOSInputVisible, setIsPOSInputVisible] = useState(true);
-  const [isFileInputVisible, setIsFileInputVisible] = useState(true);
 
   if (!isOpen) return null;
-  const size = require("../../assets/duplicate.png");
-
-  const handleCancelCpu = () => {
-    setPOSsizeValue(256); // Reset to default value
-    setIsPOSInputVisible(true);
-  };
-
-  const handleSaveCpu = () => {
-    setIsPOSInputVisible(false);
-  };
-
-  const handleCancelNonces = () => {
-    setFileSizeValue(4096); // Reset to default value
-    setIsFileInputVisible(true);
-  };
-
-  const handleSaveNonces = () => {
-    setIsFileInputVisible(false);
-  };
-
   return (
     <Backdrop onClick={onClose}>
       <Wrapper onClick={(e) => e.stopPropagation()}>
@@ -145,63 +79,7 @@ const editSize = ({ onClose, isOpen }: Props) => {
           If your PC can't prepare POST proof in a few hours, you won't get any
           rewards.
         </Subheader>
-        <BgImage src={size} />
-        <BottomContainer>
-          <TileWrapper>
-            <Tile
-              heading="Select POS data size"
-              subheader="Gibibytes"
-              footer="more data -> more rewards, but longer data generation and proving"
-            />
-            {isPOSInputVisible ? (
-              <>
-                <CustomNumberInput
-                  min={256}
-                  max={999999}
-                  fontsize={36}
-                  height={80}
-                  step={64}
-                  value={256}
-                  onChange={(val) => setPOSsizeValue(val)}
-                />
-                <SaveButton buttonLeft={55} onClick={handleSaveCpu} />
-                <CancelButton buttonLeft={45} onClick={handleCancelCpu} />
-              </>
-            ) : (
-              <>
-                <SelectedValue>{dataSizeValue}</SelectedValue>
-                <CancelButton buttonLeft={50} onClick={handleCancelCpu} />
-              </>
-            )}
-          </TileWrapper>
-          <TileWrapper>
-            <Tile
-              heading="Select file size"
-              subheader="Mebibytes"
-              footer="POS will be stored across [XXX] files" //TODO
-            />
-            {isFileInputVisible ? (
-              <>
-                <CustomNumberInput
-                  min={10}
-                  max={99999}
-                  fontsize={36}
-                  height={80}
-                  step={1}
-                  value={4096}
-                  onChange={(val) => setFileSizeValue(val)}
-                />
-                <SaveButton buttonLeft={55} onClick={handleSaveNonces} />
-                <CancelButton buttonLeft={45} onClick={handleCancelNonces} />
-              </>
-            ) : (
-              <>
-                <SelectedValue>{fileSizeValue}</SelectedValue>
-                <CancelButton buttonLeft={50} onClick={handleCancelNonces} />
-              </>
-            )}
-          </TileWrapper>
-        </BottomContainer>
+        <SetupSize/>
       </Wrapper>
     </Backdrop>
   );
