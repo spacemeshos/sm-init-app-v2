c5bb168f3994ed3dbf92950a6de4b183af2dfc41
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-06-11 21:36:58 +0200

    cleanup imports

--- src/pages/GuidedCreate/editCPU.tsx
+++ src/pages/GuidedCreate/editCPU.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Colors from "../../styles/colors";
 import { SetupProving } from "../../components/setupPOS";
