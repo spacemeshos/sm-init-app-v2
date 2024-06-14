e0add10936ee67c2473cdfd611c6adabd6378515
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-06-14 23:04:54 +0200

    fix postcli bundle

--- src/services/postcliService.js
+++ src/services/postcliService.js
@@ -2,6 +2,9 @@ import { invoke } from '@tauri-apps/api/tauri';
 
 export const callPostCli = async (args) => {
     try {
+        if (!Array.isArray(args)) {
+            throw new Error('Arguments must be an array');
+        }
         const response = await invoke('run_postcli_command', { args });
         console.log('Response from postcli:', response);
         return response;
