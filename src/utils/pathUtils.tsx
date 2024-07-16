2047d0e969b017e010daacd91db124575d8e3fff
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-07-16 21:04:58 +0200

    splin on slashes

--- src/utils/pathUtils.tsx
+++ src/utils/pathUtils.tsx
@@ -4,11 +4,28 @@
  * @param maxLength - The maximum length of the shortened path.
  * @returns The shortened path.
  */
-export const shortenPath = (path: string, maxLength: number): string => {
+export const shortenPath = (path: string, maxLength: number = 15): string => {
   if (path.length <= maxLength) {
     return path;
   }
-  const start = path.slice(0, 0); 
-  const end = path.slice(maxLength);
-  return `${start}...${end}`;
+
+  const segments = path.split("/");
+  const result = [];
+
+  let currentLength = 0;
+  for (let i = 0; i < segments.length; i++) {
+    const segment = segments[i];
+
+    // Check if adding this segment exceeds the max length
+    if (segment.length > 1 && currentLength + segment.length + 3 > maxLength) {
+      result.push("...");
+      result.push(segments[segments.length - 1]); // Always add the last segment
+      break;
+    } else {
+      result.push(segment);
+      currentLength += segment.length + 1; // Adding 1 for the slash
+    }
+  }
+
+  return result.join("/");
 };
