4b80c91506a519ead3f5daed9a94ed86e3a94c8b
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-12-02 13:41:48 +0100

    refactor to minimize code lenght

--- src/pages/Generate.tsx
+++ src/pages/Generate.tsx
@@ -12,7 +12,7 @@ import {
   SelectIdentity,
   SetupSummary,
 } from "../components/setupPOS";
-import { BackButton } from "../components/button";
+import { BackButton, Button } from "../components/button";
 
 const NavProgress = styled.div`
   width: 1200px;
@@ -21,99 +21,91 @@ const NavProgress = styled.div`
 `;
 
 const Image = styled.img`
-  height: 100vh;
-  width: 100vw;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: -1;
-  object-fit: cover;
+  position: absolute;
+  margin-bottom: 10px;
+  right: 0px;
+  top: 0px;
 `;
 
 const BottomContainer = styled.div`
+  width: 960px;
+  height: 480px;
+  position: absolute;
+  top: 200px;
+  left: 50%;
+  transform: translate(-50%);
+  background-color: ${Colors.whiteOpaque};
+  backdrop-filter: blur(8px);
   display: flex;
-  align-items: center;
+  align-items: flex-start;
+  border-radius: 10px;
   justify-content: center;
-  width: 1200px;
+  box-shadow: 0 8px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  /* Gradient border */
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-radius: 10px;
+    padding: 1px;
+    background: linear-gradient(
+      45deg,
+      ${Colors.whiteOpaque},
+      ${Colors.greenLightOpaque}
+    );
+    -webkit-mask: linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+    -webkit-mask-composite: destination-out;
+    mask-composite: exclude;
+    pointer-events: none;
+  }
 `;
 
 const SetupContainer = styled.div`
-  width: 960px;
+  width: 660px;
   height: 480px;
-  position: relative;
-  border: 0.5px solid rgba(255, 255, 255, 0.05);
-  background-color: rgba(255, 255, 255, 0.01);
-  backdrop-filter: blur(8px);
+  position: absolute;
+  left: 0px;
+  top: 0px;
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
-  border-radius: 10px;
-  box-shadow: 0 8px 10px rgba(0, 0, 0, 0.1);
 `;
 
-const MenuContainer = styled.div`
+const ButtonColumn = styled.div`
   position: absolute;
   width: 250px;
   height: 480px;
   right: 0px;
-  border: 0.5px solid rgba(255, 255, 255, 0.05);
-  border-radius: 10px;
-  background-color: rgba(255, 255, 255, 0.02);
-`;
-
-const ButtonColumn = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 20px;
-  padding: 20px;
-  justify-content: center;
+  gap: 10px;
+  justify-content: space-evenly;
+  padding-right: 10px;
 `;
 
 const TextWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-direction: row;
   justify-content: center;
-  position: absolute;
-  top: 20px;
+  position: relative;
+  top: 10px;
   left: 50%;
   transform: translateX(-50%);
-  width: 80%;
-`;
-
-const StyledButton = styled.button<{ $isActive?: boolean }>`
-  width: 200px;
-  height: 60px;
-  font-size: 16px;
-  cursor: pointer;
-  border: 0.5px solid
-    ${(props) =>
-      props.$isActive ? Colors.whiteOpaque : Colors.greenLightOpaque};
-  background-color: ${Colors.whiteOpaque};
-  border-radius: 10px;
-  transition: all 0.2s ease;
-  font-family: "Source Code Pro Extralight", sans-serif;
-  color: ${Colors.white};
-  font-size: 14px;
-  font-weight: 300;
-  text-transform: uppercase;
-  letter-spacing: 3px;
-  line-height: 20px;
-
-  &:hover {
-    background-color: ${Colors.greenLightOpaque};
-    border: 1px solid ${Colors.whiteOpaque};
-  }
 `;
 
 const ContentContainer = styled.div`
-  width: 960px;
+  width: 660px;
   height: 480px;
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: 20px;
 `;
 
 const Generate: React.FC = () => {
@@ -181,27 +173,26 @@ const Generate: React.FC = () => {
           </TextWrapper>
 
           <ContentContainer>{renderContent()}</ContentContainer>
-
-          <MenuContainer>
-            <ButtonColumn>
-              {steps.map((step, index) => (
-                <StyledButton
-                  key={index}
-                  onClick={() => handleStepChange(index)}
-                  $isActive={currentStep === index}
-                >
-                  {step.label}
-                </StyledButton>
-              ))}
-              <StyledButton
-                onClick={() => setShowSummary(true)}
-                $isActive={showSummary}
-              >
-                Summary
-              </StyledButton>
-            </ButtonColumn>
-          </MenuContainer>
         </SetupContainer>
+          <ButtonColumn>
+            {steps.map((step, index) => (
+              <Button
+                key={index}
+                onClick={() => handleStepChange(index)}
+                $isActive={currentStep === index}
+                label={step.label}
+                width={250}
+              ></Button>
+            ))}
+            <Button
+              onClick={() => setShowSummary(true)}
+              $isActive={showSummary}
+              width={250}
+              label="Summary"
+            >
+              Summary
+            </Button>
+          </ButtonColumn>
       </BottomContainer>
     </>
   );
