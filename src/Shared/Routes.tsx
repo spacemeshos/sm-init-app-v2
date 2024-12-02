a1527b3e98c43f565f7d2c6c1ebb4eb9606a6b48
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-12-02 13:24:40 +0100

    WIP - switch to one screen setup

--- src/Shared/Routes.tsx
+++ src/Shared/Routes.tsx
@@ -1,13 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import SelectDirectory from "../pages/GuidedCreate/ScreenSelectDirectory";
-import TailoredSettings from "../pages/GuidedCreate/ScreenTailoredSettings";
-import Summary from "../pages/GuidedCreate/ScreenSummary";
-import AdvSelectDirectory from "../pages/AdvancedCreate/ScreenAdvDirectory";
-import AdvSetupSize from "../pages/AdvancedCreate/ScreenAdvSize";
-import AdvSetupProving from "../pages/AdvancedCreate/ScreenAdvProving";
-import AdvSetupProvider from "../pages/AdvancedCreate/ScreenAdvProvider";
 import Confirmation from "../components/confirmation";
 import Generate from "../pages/Generate";
 
@@ -16,13 +9,6 @@ const AppRoutes = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/generate" element={<Generate />} />
-      <Route path="/guided/SelectDirectory" element={<SelectDirectory />} />
-      <Route path="/guided/TailoredSettings" element={<TailoredSettings />} />
-      <Route path="/guided/Summary" element={<Summary />} />
-      <Route path="/advanced/Directory" element={<AdvSelectDirectory />} />
-      <Route path="/advanced/SetupSize" element={<AdvSetupSize />} />
-      <Route path="/advanced/Proving" element={<AdvSetupProving />} />
-      <Route path="/advanced/Provider" element={<AdvSetupProvider />} />
       <Route path="/guided/Confirmation" element={<Confirmation />} />
     </Routes>
   </BrowserRouter>
