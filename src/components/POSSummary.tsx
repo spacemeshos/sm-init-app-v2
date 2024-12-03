f40e47e88f42e8fcc6fa1c648d9e079faf99d3fd
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-12-03 22:57:35 +0100

    fix layout

--- src/components/POSSummary.tsx
+++ src/components/POSSummary.tsx
@@ -10,32 +10,19 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 1200px;
-  height: 600px;
-  position: relative;
-  padding: 20px;
+  justify-content: space-evenly;
+  width: 660px;
+  height: 400px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  margin: 60px 15px 20px 15px;
 `;
 
 const SummarySection = styled.div`
-  width: 800px;
-  margin-bottom: 40px;
-`;
-
-const Title = styled.h1`
-  color: ${Colors.greenLight};
-  font-family: "Source Code Pro", sans-serif;
-  font-size: 24px;
-  font-weight: 300;
-  text-align: center;
-  text-transform: uppercase;
-  letter-spacing: 3px;
-  margin-bottom: 40px;
-`;
-
-const ButtonContainer = styled.div`
-  position: absolute;
-  bottom: 40px;
-  right: 40px;
+  width: 660px;
+  height: 250px;
+  position: relative;
 `;
 
 interface POSSummaryProps {
@@ -56,62 +43,51 @@ const POSSummary: React.FC<POSSummaryProps> = ({ onProceed }) => {
 
   return (
     <Container>
-      <Title>Setup Summary</Title>
-
       <SummarySection>
         <Frame
-          height={60}
-          heading="Storage Location"
+          height={20}
+          heading="POS Location"
           subheader={shortenPath(settings.selectedDir || "", 30)}
         />
-      </SummarySection>
-
-      <SummarySection>
         <Frame
-          height={60}
-          heading="Space Configuration"
+          height={20}
+          heading="POS Size"
           subheader={`${
             settings.numUnits || 4
           } Space Units (${calculateTotalSize()})`}
         />
-      </SummarySection>
-
-      <SummarySection>
         <Frame
-          height={60}
-          heading="Processing Setup"
-          subheader={`Provider ID: ${settings.provider || 0} | ${
-            settings.numCores || 8
-          } CPU Cores | ${settings.numNonces || 288} Nonces`}
+          height={20}
+          heading="POS Provider"
+          subheader={`Provider ID: ${settings.provider || 0}`}
+        />
+        <Frame
+          height={20}
+          heading="Proving Setup"
+          subheader={`${settings.numCores || 8} CPU Cores | ${
+            settings.numNonces || 288
+          } Nonces`}
+        />
+        <Frame
+          height={20}
+          heading="Identity Configuration"
+          subheader={
+            settings.identityFile
+              ? `File: ${shortenPath(settings.identityFile, 20)}`
+              : settings.atxId
+              ? `ATX ID: ${settings.atxId.substring(0, 10)}...`
+              : ""
+          }
         />
       </SummarySection>
-
-      {(settings.identityFile || settings.atxId) && (
-        <SummarySection>
-          <Frame
-            height={60}
-            heading="Identity Configuration"
-            subheader={
-              settings.identityFile
-                ? `File: ${shortenPath(settings.identityFile, 20)}`
-                : settings.atxId
-                ? `ATX ID: ${settings.atxId.substring(0, 10)}...`
-                : ""
-            }
-          />
-        </SummarySection>
-      )}
-
-      <ButtonContainer>
-        <Button
-          label="Proceed with Setup"
-          onClick={onProceed}
-          width={250}
-          height={60}
-          backgroundColor={Colors.darkerPurple}
-          borderColor={Colors.purpleLight}
-        />
-      </ButtonContainer>
+      <Button
+        label="Generate POS Data"
+        onClick={onProceed}
+        width={250}
+        height={56}
+        backgroundColor={Colors.darkerPurple}
+        borderColor={Colors.purpleLight}
+      />
     </Container>
   );
 };
