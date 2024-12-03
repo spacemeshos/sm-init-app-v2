2d25c55139dbbf9a603434c1c02177e10285cdce
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-12-03 22:58:02 +0100

    simplified code

--- src/components/frames.tsx
+++ src/components/frames.tsx
@@ -5,7 +5,6 @@ import Colors from "../styles/colors";
 interface FrameProps {
   heading?: string;
   subheader?: string;
-  footer?: string;
   children?: React.ReactNode;
   buttonText?: string;
   onClick?: () => void;
@@ -13,7 +12,6 @@ interface FrameProps {
   left?: number;
   height?: number;
   width?: number;
-  borderColor?: string;
 }
 
 const Wrapper = styled.div<{
@@ -21,9 +19,8 @@ const Wrapper = styled.div<{
   left?: number;
   height?: number;
   width?: number;
-  borderColor?: string;
 }>`
-  background-color: ${Colors.background};
+  background-color: ${Colors.whiteOpaque};
   position: relative;
   top: ${({ top }) => top}px;
   left: ${({ left }) => left}px;
@@ -33,25 +30,16 @@ const Wrapper = styled.div<{
   justify-content: center;
   flex-direction: row;
   align-items: center;
-  border: 1px solid ${({ borderColor }) => borderColor || "transparent"};
-  border-image: ${({ borderColor }) =>
-    borderColor
-      ? "none"
-      : `linear-gradient(
-          90deg,
-          ${Colors.greenLight} 0%,
-          ${Colors.greenDark} 50%,
-          ${Colors.greenLight} 100%
-        ) 1`};
+  border: 0.5px solid ${Colors.greenLightOpaque};
 `;
 
 const FrameHeading = styled.h2`
-  color: ${Colors.grayLight};
+  color: ${Colors.white};
   font-family: "Source Code Pro", sans-serif;
   left: 20px;
   text-align: left;
   text-transform: uppercase;
-  font-size: 18px;
+  font-size: 16px;
   font-weight: 100;
   letter-spacing: 3px;
   width: 100%;
@@ -59,26 +47,13 @@ const FrameHeading = styled.h2`
 `;
 
 const SubHeader = styled.h3`
-  color: ${Colors.grayLight};
+  color: ${Colors.white};
   font-family: "Source Code Pro ExtraLight", sans-serif;
   position: absolute;
   text-align: right;
   right: 20px;
   text-transform: uppercase;
-  font-size: 16px;
-  font-weight: 100;
-  letter-spacing: 3px;
-  width: 100%;
-`;
-
-const Footer = styled.h3`
-  color: ${Colors.grayLight};
-  font-family: "Source Code Pro ExtraLight", sans-serif, Arial;
-  bottom: 5%;
-  position: absolute;
-  text-align: center;
-  text-transform: uppercase;
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 100;
   letter-spacing: 3px;
   width: 100%;
@@ -87,13 +62,11 @@ const Footer = styled.h3`
 const Frame: React.FC<FrameProps> = ({
   heading,
   subheader,
-  footer,
   children,
   height,
   left,
   top,
   width,
-  borderColor,
 }) => {
   return (
     <Wrapper
@@ -101,11 +74,9 @@ const Frame: React.FC<FrameProps> = ({
       left={left}
       height={height}
       width={width}
-      borderColor={borderColor}
     >
       <FrameHeading>{heading}</FrameHeading>
       <SubHeader>{subheader}</SubHeader>
-      <Footer>{footer}</Footer>
       {children}
     </Wrapper>
   );
