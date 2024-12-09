35d7f20abc4c76cc7f8fdd8beea0de619c3b2213
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-12-09 23:29:30 +0100

    integrate console components and context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-/* eslint-disable react/react-in-jsx-scope */
+import React from "react";
 import styled from "styled-components";
 import Colors from "./styles/colors";
 import AppRoutes from "./Shared/Routes";
 import { ExternalLinks } from "./Shared/Constants";
 import { open } from "@tauri-apps/api/shell";
 import GlobalStyles from "./styles/globalStyles";
+import { ConsoleProvider } from "./state/ConsoleContext";
+import ConsoleView from "./components/ConsoleView";
+import ConsoleTest from "./components/ConsoleTest";
 
 const AppWrapper = styled.div`
   height: 100%;
@@ -13,11 +16,30 @@ const AppWrapper = styled.div`
   background-color: ${Colors.greenDark};
 `;
 
+const ConsoleWrapper = styled.div`
+  position: fixed;
+  bottom: 40px;
+  left: 50px;
+  right: 50px;
+  z-index: 1000;
+  background-color: ${Colors.black}80;
+  padding: 10px;
+  border-radius: 8px;
+  pointer-events: auto;
+`;
+
+const TestWrapper = styled.div`
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  z-index: 1000;
+`;
+
 const Footer = styled.div`
   position: fixed;
   width: 1100px;
   height: 30px;
-  top: 690px;
+  bottom: 10px;
   left: 50px;
   display: flex;
   flex-direction: row;
@@ -29,15 +51,23 @@ const Footer = styled.div`
   font-size: 12px;
   font-weight: 100;
   letter-spacing: 1px;
+  z-index: 1001;
 `;
 
-function App() {
+// Wrapper component to use hooks
+const AppContent: React.FC = () => {
   const navigateToIssue = () => open(ExternalLinks.Report);
 
   return (
     <AppWrapper>
       <GlobalStyles />
+      <TestWrapper>
+        <ConsoleTest />
+      </TestWrapper>
       <AppRoutes />
+      <ConsoleWrapper>
+        <ConsoleView />
+      </ConsoleWrapper>
       <Footer>
         <a
           onClick={navigateToIssue}
@@ -49,6 +79,14 @@ function App() {
         </a>
       </Footer>
     </AppWrapper>
+  );
+};
+
+function App() {
+  return (
+    <ConsoleProvider>
+      <AppContent />
+    </ConsoleProvider>
   );
 }
 
