12b28f5ed0c8d92b033c890ec30a041c5fa15165
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-12-14 23:59:58 +0100

    feat(styles): add darkOpaque color for enhanced styling options fix(styles): correct gradientGB color order for visual consistency

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -6,11 +6,12 @@ const Colors = {
   blueLight: "#55E8E2",
   white: "#FFFFFF",
   whiteOpaque: "rgba(255, 255, 255, 0.05)",
+  darkOpaque: "rgba(6, 26, 20, 0.05)",
   grayLight: "#F0F0F0",
   grayMedium: "#727b80",
   red: "#EC0000",
   black: "#000000",
-  gradientGB: "linear-gradient(87.93deg, #3AFFA7 2.84%, #55E8E2 97.16%)",
+  gradientGB: "linear-gradient(87.93deg, #55E8E2 2.84%, #3AFFA7 97.16%)",
 };
   
   export default Colors;
