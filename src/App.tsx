42089f65e336ab3e2420871d5540b76532399118
Monika Smolarek <ms.monikasmolarek@gmail.com>
2024-12-15 01:36:17 +0100

    refactor(App.tsx): simplify component structure by removing unnecessary AppContent wrapper and Footer component, and adjust AppWrapper and ConsoleWrapper to use full width and height

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { open } from "@tauri-apps/api/shell";
 import React from "react";
 import styled from "styled-components";
 
 import ConsoleView from "./components/ConsoleView";
-import { ExternalLinks } from "./Shared/Constants";
 import AppRoutes from "./Shared/Routes";
 import { ConsoleProvider } from "./state/ConsoleContext";
 import Colors from "./styles/colors";
 import GlobalStyles from "./styles/globalStyles";
 
 const AppWrapper = styled.div`
-  height: 800px;
-  width: 1300px;
+  height: 100%;
+  width: 100%;
   position: absolute;
   background-color: ${Colors.greenDark};
 `;
@@ -20,55 +18,24 @@ const ConsoleWrapper = styled.div`
   position: fixed;
   left: 0px;
   bottom: 0px;
-  width: 1300px;
+  width: 100%;
   z-index: 1;
   pointer-events: auto;
 `;
 
-const Footer = styled.div`
-  position: fixed;
-  width: 1300px;
-  height: 30px;
-  bottom: 0px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-`;
-
 // Wrapper component to use hooks
-const AppContent: React.FC = () => {
-  const navigateToIssue = () => open(ExternalLinks.Report);
-
+const App: React.FC = () => {
   return (
-    <AppWrapper>
-      <GlobalStyles />
-      <AppRoutes />
-      <Footer>
+    <ConsoleProvider>
+      <AppWrapper>
+        <GlobalStyles />
+        <AppRoutes />
         <ConsoleWrapper>
           <ConsoleView />
         </ConsoleWrapper>
-
-        <a
-          onClick={navigateToIssue}
-          role="button"
-          tabIndex={0}
-          style={{ cursor: "pointer" }}
-        >
-          report an issue
-        </a>
-      </Footer>
-    </AppWrapper>
-  );
-};
-
-function App() {
-  return (
-    <ConsoleProvider>
-      <AppContent />
+      </AppWrapper>
     </ConsoleProvider>
   );
-}
+};
 
 export default App;
