68310c94424b405d66d9fbde7740c4565949b188
Monika Smolarek <ms.monikasmolarek@gmail.com>
2025-01-21 10:07:37 +0100

    Refactor modal backdrop color to use centralized color variable

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -10,12 +10,12 @@ const Backdrop = styled.div<{ isOpen?: boolean }>`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%; 
+  width: 100%;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: ${Colors.backdropColor};
   z-index: ${({ isOpen }) => (isOpen ? 9999 : -1)};
 `;
 
