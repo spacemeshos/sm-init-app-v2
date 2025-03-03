77b23f737e5c9ba1f543080c5a97c4aa403ad0c3
Monika Smolarek <ms.monikasmolarek@gmail.com>
2025-03-03 10:01:03 +0100

    remove POSSummary component and its exports

--- src/components/pos/index.tsx
+++ src/components/pos/index.tsx
@@ -4,4 +4,3 @@ export { SetupFileSize } from "./SetupSize";
 export { SetupGPU } from "./SetupGPU";
 export { SelectIdentity } from "./SelectIdentity";
 export { SelectATX } from "./SelectATX";
-export { POSSummary } from "./POSSummary";
